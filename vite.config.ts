175d778cb314e216c49745fbf444e3ab93839cb2
ferzos <f-chandra@mercari.com>
2025-01-01 12:26:32 +0900

    chore(vite): install require-transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), requireTransform({})],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
