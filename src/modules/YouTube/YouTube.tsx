f52bd747c6e6be32f49da25ea546e87608aea0db
ferzos <f-chandra@mercari.com>
2025-01-04 20:11:21 +0900

    chore(vite): install @ap.cx/react-fullpage

--- src/modules/YouTube/YouTube.tsx
+++ src/modules/YouTube/YouTube.tsx
@@ -1,28 +1,32 @@
 import ReactYouTube from 'react-youtube';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FullpageSection } from '@ap.cx/react-fullpage';
 import './YouTube.css';
 
 export function YouTube() {
   return (
-    <div className="ytContainer">
-      <p className="ytSectionTitle">Josephus Problem</p>
+    <FullpageSection>
+      <div className="ytContainer">
+        <p className="ytSectionTitle">Josephus Problem</p>
 
-      <ReactYouTube
-        iframeClassName="iframeContainer"
-        videoId="uCsD3ZGzMgE"
-        id="uCsD3ZGzMgE"
-        title="The Josephus Problem - Numberphile"
-        loading="eager"
-        opts={{
-          width: window.innerWidth * 0.8,
-          height: window.innerHeight * 0.8,
-          playerVars: {
-            end: 48,
-            controls: 0,
-            disablekb: 1,
-          },
-        }}
-      />
-    </div>
+        <ReactYouTube
+          iframeClassName="iframeContainer"
+          videoId="uCsD3ZGzMgE"
+          id="uCsD3ZGzMgE"
+          title="The Josephus Problem - Numberphile"
+          loading="eager"
+          opts={{
+            width: window.innerWidth * 0.8,
+            height: window.innerHeight * 0.8,
+            playerVars: {
+              end: 48,
+              controls: 0,
+              disablekb: 1,
+            },
+          }}
+        />
+      </div>
+    </FullpageSection>
   );
 }
 
