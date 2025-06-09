e77481a826c74aee81bc65e3702a2bc2b503c307
ferzos <f-chandra@mercari.com>
2025-06-09 21:22:37 +0900

    chore: remove eslint disable comment for extraneous dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './styles/reset.css';
 import './App.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import Fullpage, { FullPageSections } from '@ap.cx/react-fullpage';
 
 import { YouTube } from './modules/YouTube';
