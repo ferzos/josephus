da72eeba9f2720c87f16ff1bb148bb724251ea4e
ferzos <f-chandra@mercari.com>
2025-06-09 21:24:32 +0900

    feat: integrate framer-motion for animations and enhance kill sequence management

--- src/modules/Josephus/hooks/useDeadSequence.ts
+++ src/modules/Josephus/hooks/useDeadSequence.ts
@@ -1,32 +1,69 @@
+import { useAtomValue } from 'jotai';
 import { useEffect, useState } from 'react';
+import { killersAtom } from './useKillRotation';
 
 interface DeadSequenceItem {
   index: number;
   hasShownDead: boolean;
 }
 
+interface KillSequenceItem {
+  index: number;
+  killerIndex: number;
+  haShownKilling: boolean;
+}
+
+const DEAD_SEQUENCE_DELAY = 500;
+
 export const useDeadSequence = () => {
   const [deadSequence, setDeadSequence] = useState<DeadSequenceItem[]>([]);
+
+  const killers = useAtomValue(killersAtom);
+  const [killSequence, setKillSequence] = useState<KillSequenceItem[]>([]);
+
+  useEffect(() => {
+    if (killers.length === 0) return;
+
+    setKillSequence(
+      killers.map((killerIndex, index) => ({
+        index,
+        killerIndex,
+        haShownKilling: false,
+      })),
+    );
+  }, [killers]);
 
   useEffect(() => {
     if (deadSequence.every((dead) => dead.hasShownDead)) return;
 
     const nextInDeadSequence = deadSequence.find((dead) => !dead.hasShownDead);
+    const nextInKillSequence = killSequence.find(
+      (killer) => !killer.haShownKilling,
+    );
 
-    setTimeout(
-      () => setDeadSequence((currDeadSequence) => currDeadSequence.map((deadSequenceItem) => ({
+    setTimeout(() => {
+      setKillSequence((currKillSequence) => currKillSequence.map((killSequenceItem) => ({
+        ...killSequenceItem,
+        haShownKilling:
+            killSequenceItem.index === nextInKillSequence?.index
+              ? true
+              : killSequenceItem.haShownKilling,
+      })));
+
+      setDeadSequence((currDeadSequence) => currDeadSequence.map((deadSequenceItem) => ({
         ...deadSequenceItem,
         hasShownDead:
-              nextInDeadSequence?.index === deadSequenceItem.index
-                ? true
-                : deadSequenceItem.hasShownDead,
-      }))),
-      500,
-    );
-  }, [deadSequence]);
+            nextInDeadSequence?.index === deadSequenceItem.index
+              ? true
+              : deadSequenceItem.hasShownDead,
+      })));
+    }, DEAD_SEQUENCE_DELAY);
+  }, [deadSequence, killSequence, killers]);
 
   return {
     deadSequence,
     setDeadSequence,
+    killSequence,
+    setKillSequence,
   };
 };
